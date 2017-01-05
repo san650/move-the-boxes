21add2903861bf444ee4c451b46c203a46ccc053
Santiago Ferreira <san650@gmail.com>
2017-01-05 20:27:29 -0300

    Add pollock level

--- app/models/levels/6-pollock.js
+++ app/models/levels/6-pollock.js
@@ -1,70 +1,30 @@
 import Ember from 'ember';
 import Level from 'sokoban/models/level';
-import {
-  createArrow,
-  createBoard,
-  createBox,
-  createPlayer,
-  createTarget,
-  createWall,
-  createWater
-} from 'sokoban/models/factory';
+import Forgery from 'sokoban/mixins/forgery';
 
 const { computed } = Ember;
 
-export default Level.extend({
+const MAP = `
+        XXXXXXXX 
+        X     @X 
+        X *X* XX 
+        X *  *X  
+        XX* * X  
+XXXXXXXXX * X XXX
+X....  XX *  *  X
+XX...    *  *   X
+X....  XXXXXXXXXX
+XXXXXXXX 
+`;
+
+export default Level.extend(Forgery, {
   minMoves: 13,
 
   board: computed(function() {
-    let map = `
-XXXXX   XXXXXXXXXXXXX
-XXXXX*  XXXXXXXXXXXXX
-XXXXX  *XXXXXXXXXXXXX
-XXX  *  * XXXXXXXXXXX
-XXX X XXX XXXXXXXXXXX
-X   X XXX XXXXXXX  ..
-X *  *             ..
-XXXXX XXXX X@XXXX  ..
-XXXXX      XXX  XXXXX`;
-
-    let lines = map
-      .split('\n')
-      .filter(s => !/^\s*$/.test(s))
-      .map(s => s.replace(/^\s*/, ''));
-
-    let board = createBoard(lines.length, lines[0].length);
-
-    for(let j = 0; j < lines.length; j++) {
-      for(let i = 0; i < lines[j].length; i++) {
-        let c = lines[j][i];
-
-        switch(c) {
-          case 'X':
-            board.pushCell(createWall(j, i));
-          break;
-          case '.':
-            board.pushCell(createTarget(j, i));
-          break;
-          case '*':
-            board.pushCell(createBox(j, i));
-          break;
-          case 'w':
-            board.pushCell(createWater(j, i));
-          break;
-          case '>':
-            board.pushCell(createArrow(j, i, 'right'));
-          break;
-          case '<':
-            board.pushCell(createArrow(j, i, 'left'));
-          break;
-        }
-      }
-    }
-
-    return board;
+    return this.boardFromString(MAP);
   }),
 
   player: computed(function() {
-    return createPlayer(7, 12);
+    return this.playerFromString(MAP);
   })
 });
