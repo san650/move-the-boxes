3f2aa7f1b92ccd61c676939df4db243e2db328bf
Santiago Ferreira <san650@gmail.com>
2017-01-06 15:05:36 -0300

    Always use zoom

--- app/components/draw-board.js
+++ app/components/draw-board.js
@@ -9,15 +9,8 @@ const Component = Ember.Component.extend({
   style: computed('board.{rowCount,columnCount}', function() {
     let width = SIZE * this.get('board.columnCount');
     let height = SIZE * this.get('board.rowCount');
-    let zoom = this.get('zoom');
 
-    if (zoom !== 1) {
-      zoom = `transform:scale(${zoom});transform-origin(0,0);`;
-    } else {
-      zoom = "";
-    }
-
-    return `width:${width}px;height:${height}px;${zoom}`;
+    return `width:${width}px;height:${height}px;`;
   })
 });
 
